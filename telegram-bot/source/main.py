204bf51b49d989e24bf95015851e916e90b534d0
Maxim Khabarov <43231178+haboh@users.noreply.github.com>
2024-11-13 11:31:43 +0300

    add time scheduling

--- telegram-bot/source/main.py
+++ telegram-bot/source/main.py
@@ -6,8 +6,12 @@ with open("token.txt", "r") as f:
 bot = telebot.TeleBot(TOKEN)
 
 command_list_help = ["/start - начать работу",
-                     "/help - вывести список команд",
-                     "/exit - завершить работу"]
+    "/help - вывести список команд",
+    "/setTime hh:mm - установить время отправки дайджеста (в формате час:минута)",
+    "/setPeriod n - установить частоту отправки дайджеста (n в днях)",
+    "/digest - получить дайджест",
+    "/settings - вывести команды для пользовательских настроек",
+    "/exit - завершить работу",]
 is_started = False
 
 
@@ -40,5 +44,31 @@ def exit_bot(message):
     if is_started:
         exit()
 
+timesToSend = []
+periodsToSend = []
+
+def clockWatcherRoutine():
+    while True:
+        sleep(1)
+        for user_id, hour, minute in timesToSend:
+           if hour == datetime.datetime.now().hour and minute == datetime.datetime.now().minute:
+               send_digest(user_id)
+
+
+clockWatcher = Thread(target = clockWatcherRoutine)
+clockWatcher.setDaemon(True)
+clockWatcher.start()
+
+@bot.message_handler(commands=["setTime"])
+def setTime_bot(message):
+    user_id = message.from_user.id
+    date = message.text.split(":")
+    timesToSend.append((user_id, int(date[0]), int(date[1])))
+
+@bot.message_handler(commands=["setPeriod"])
+def setPeriod_bot(message):
+    user_id = message.from_user.id
+    period = message.text
+    periodsToSend.append(period)
 
 bot.infinity_polling()
